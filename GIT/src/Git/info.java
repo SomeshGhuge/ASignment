956d9b47f455dd431c4fcab64021212dd06fd77c
HP <someshghuge85@gmail.com>
2022-10-03 20:02:42 +0530

    first

---
+++ GIT/src/Git/info.java
@@ -0,0 +1,26 @@
+package Git;
+
+public class info {
+    
+//	GIT    :----- 
+//            it is a version control mechanism.
+//	          it is used to design minor and major projects with high speed and efficiency.
+//	          It is developed to co-ordinate the work among the developers. 
+//	          The version control allows us to track and work together with our team members
+//	          at the same workspace
+//	         
+//	GITHub :-----
+//	          it is a distributed version controll system.
+//	          Central version controll system.
+//            GitHub is an online software development platform
+//	          used for storing, tracking, and collaborating on software projects.
+//	         
+//	SCM    :-----
+//	          Source code management
+//	          Source code management (SCM) is used to track modifications to a 
+//	          source code repository. SCM tracks a running history of changes to a
+//	          code base and helps resolve conflicts when merging updates from multiple
+//	          contributors.
+	               
+	
+}
